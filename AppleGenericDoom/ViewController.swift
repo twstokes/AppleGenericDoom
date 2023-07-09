e73659a85a43d73b2771e1d07cbfe943f5dca9a8
Tanner W. Stokes <tanner@tannr.com>
2023-07-08 22:37:38 -0400

    Initial commit.

---
+++ AppleGenericDoom/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppleGenericDoom
+//
+//  Created by Tanner W. Stokes on 7/8/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
