72c659272e873cc4b642f6ce95d39434bfd6bc9b
Tanner W. Stokes <tanner@tannr.com>
2023-07-08 22:43:49 -0400

    Add Apple driver boilerplate.

---
+++ AppleGenericDoom/doomgeneric_apple.c
@@ -0,0 +1,70 @@
+#include "doomkeys.h"
+#include "m_argv.h"
+#include "doomgeneric.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <stdbool.h>
+
+#define KEYQUEUE_SIZE 16
+
+static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
+static unsigned int s_KeyQueueWriteIndex = 0;
+static unsigned int s_KeyQueueReadIndex = 0;
+
+static unsigned char convertToDoomKey(unsigned int key){
+    return 0;
+}
+
+static void addKeyToQueue(int pressed, unsigned int keyCode){
+  unsigned char key = convertToDoomKey(keyCode);
+
+  unsigned short keyData = (pressed << 8) | key;
+
+  s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+  s_KeyQueueWriteIndex++;
+  s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+}
+static void handleKeyInput(){
+}
+
+
+void DG_Init(){
+}
+
+void DG_DrawFrame()
+{
+}
+
+void DG_SleepMs(uint32_t ms)
+{
+}
+
+uint32_t DG_GetTicksMs()
+{
+    return 0;
+}
+
+int DG_GetKey(int* pressed, unsigned char* doomKey)
+{
+  if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex){
+    //key queue is empty
+    return 0;
+  }else{
+    unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
+    s_KeyQueueReadIndex++;
+    s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
+
+    *pressed = keyData >> 8;
+    *doomKey = keyData & 0xFF;
+
+    return 1;
+  }
+
+  return 0;
+}
+
+void DG_SetWindowTitle(const char * title)
+{
+}
