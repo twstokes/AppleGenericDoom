60a96bfd56d5efd2fb47c32551df742fdc533f93
Tanner W. Stokes <tanner@tannr.com>
2023-07-12 12:43:09 -0400

    Cleanup.

--- AppleGenericDoom/doomgeneric_objc_interop.m
+++ AppleGenericDoom/doomgeneric_objc_interop.m
@@ -9,14 +9,11 @@
 
 #if TARGET_OS_WATCH
 #import <AppleGenericDoomWatch_Watch_App-Swift.h>
-#endif
-
-#if TARGET_OS_OSX
+#elif TARGET_OS_OSX
 #import <AppleGenericDoom-Swift.h>
 #endif
 
 #include "doomkeys.h"
-//#include "m_argv.h"
 #include "doomgeneric.h"
 
 #include <stdio.h>
@@ -24,31 +21,8 @@
 
 #include <stdbool.h>
 
-#define KEYQUEUE_SIZE 16
-
-static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
-static unsigned int s_KeyQueueWriteIndex = 0;
-static unsigned int s_KeyQueueReadIndex = 0;
 static DoomGenericSwift *dgs;
 static CFAbsoluteTime timeInSeconds = 0;
-
-
-static unsigned char convertToDoomKey(unsigned int key){
-    return 0;
-}
-
-static void addKeyToQueue(int pressed, unsigned int keyCode){
-    unsigned char key = convertToDoomKey(keyCode);
-
-    unsigned short keyData = (pressed << 8) | key;
-
-    s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
-    s_KeyQueueWriteIndex++;
-    s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
-}
-
-static void handleKeyInput(){
-}
 
 void DG_Init()
 {
@@ -73,21 +47,7 @@ uint32_t DG_GetTicksMs()
 
 int DG_GetKey(int* pressed, unsigned char* doomKey)
 {
-  if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex){
-    //key queue is empty
     return 0;
-  }else{
-    unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
-    s_KeyQueueReadIndex++;
-    s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
-
-    *pressed = keyData >> 8;
-    *doomKey = keyData & 0xFF;
-
-    return 1;
-  }
-
-  return 0;
 }
 
 void DG_SetWindowTitle(const char * title)
